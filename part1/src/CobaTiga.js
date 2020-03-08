5daefbf3c96a1e835b206d8c713cc9fc91199e0c
Nasution <46909190+nasutioncode@users.noreply.github.com>
2020-03-08 23:57:44 +0700

    part1 begin

--- part1/src/CobaTiga.js
+++ part1/src/CobaTiga.js
@@ -16,7 +16,7 @@ function CobaDua() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Mas
+          Learn React Mas, I love You
         </a>
       </header>
     </div>
